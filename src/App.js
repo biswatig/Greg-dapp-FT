13b029256ec10182879f6b38eb33bff190614ae6
biswatig <dbiswatig@gmail.com>
2023-08-12 21:32:37 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+
+// Components
+import Navigation from './components/Navigation';
+import Search from './components/Search';
+import Home from './components/Home';
+
+// ABIs
+import RealEstate from './abis/RealEstate.json'
+import Escrow from './abis/Escrow.json'
+
+// Config
+import config from './config.json';
+
+function App() {
+
+  const [account, setAccount] = useState(null)
+
+  const loadBlockchainData = async() => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts'});
+    setAccount(account[0])
+    console.log(accounts[0])
+  }
+
+  useEffect(()=>{
+    loadBlockchainData()
+  },[])
+  return (
+    <div>
+
+      <div className='cards__section'>
+
+        <h3>Welcome to Millow !</h3>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
